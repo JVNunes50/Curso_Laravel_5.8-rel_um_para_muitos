304d03245e2734e4edbf616c99750b561c8ee975
JVNunes50 <jvnunes1944@gmail.com>
2025-07-16 21:28:40 -0300

    Utilizando o belongsTo

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,41 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Produto;
+use App\Categoria;
+
+Route::get('/categorias', function () {
+    $cat = Categoria::all();
+    if(count($cat) == 0){
+        echo"<h4>Voce nao possui nenhuma categoria cadastrada</h4>";
+    }else{
+        foreach($cat as $c){
+            echo"<p>" . $c->id . " - " . $c->nome . "</p>";
+        }
+    }
+});
+
+Route::get('/produtos', function () {
+    $prod = Produto::all();
+    if(count($prod) == 0){
+        echo"<h4>Voce nao possui nenhum produto cadastrado</h4>";
+    }else{
+        echo"<table>";
+        echo"<thead>
+        <tr>
+        <td>Nome</td>
+        <td>Preco</td>
+        <td>Estoque</td>
+        <td>Categoria</td>
+        </tr>
+        </thead>";
+        foreach($prod as $p){
+            echo"<tr>";
+            echo"<td>" . $p->nome . "</td>";
+            echo"<td>" . $p->preco . "</td>";
+            echo"<td>" . $p->estoque . "</td>";
+            echo"<td>" . $p->categoria->nome . "</td>";
+            echo"</tr>";
+        }
+    }
 });
